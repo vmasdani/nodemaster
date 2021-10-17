398567e496d127c4c7aaf807ed92b65bc1e98a32
vmasdani <valianmasdani@gmail.com>
2021-10-17 18:02:56 +0700

    add codegen

---
+++ codegen.ts
@@ -0,0 +1,60 @@
+import fs, { readdir, writeFile, lstatSync } from "fs";
+import util from "util";
+
+const template = `// === GENERATEED CODE === do not modify by hand.
+import express from "express";
+import dotenv from "dotenv";
+import fs, { lstatSync, existsSync } from "fs";
+import util from "util";
+#importlist
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.json({
+    hello: "world!",
+    apps: (await util.promisify(fs.readdir)("./apps"))
+        .filter(dir => lstatSync('./apps/' + dir).isDirectory() && existsSync('./apps/' + dir + '/' + 'index.ts'))
+        .map(dir => ({
+            name: dir,
+            path: process.env.BASE_URL + "/" + dir
+        })),
+  });
+});
+
+#applist
+
+app.listen(3000);`;
+
+const main = async () => {
+  try {
+    util.promisify(writeFile)(
+      "./index.ts",
+      template
+        .replace(
+          "#importlist",
+          (await util.promisify(readdir)("./apps"))
+          .filter((dir) => fs.lstatSync(`./apps/${dir}`).isDirectory() && fs.existsSync(`./apps/${dir}/index.ts`) )
+          .map((dir) => {
+              return `import ${dir} from './apps/${dir}'`;
+            })
+            .join("\n")
+        )
+        .replace(
+          "#applist",
+          (await util.promisify(readdir)("./apps"))
+            .filter((dir) => fs.lstatSync(`./apps/${dir}`).isDirectory() && fs.existsSync(`./apps/${dir}/index.ts`) )
+            .map((dir) => {
+              return `app.use("/${dir}", ${dir})`;
+            })
+            .join("\n")
+        )
+    );
+  } catch (e) {
+    console.error("Error writing index file!", e);
+  }
+};
+
+main();
--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
+// === GENERATEED CODE === do not modify by hand.
 import express from "express";
-import { cwd } from "process";
+import dotenv from "dotenv";
+import fs, { lstatSync, existsSync } from "fs";
+import util from "util";
+import app1 from './apps/app1'
+import app2 from './apps/app2'
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", async (req, res) => {
   res.json({
     hello: "world!",
-    dirname: __dirname,
-    cwd: cwd(),
+    apps: (await util.promisify(fs.readdir)("./apps"))
+        .filter(dir => lstatSync('./apps/' + dir).isDirectory() && existsSync('./apps/' + dir + '/' + 'index.ts'))
+        .map(dir => ({
+            name: dir,
+            path: process.env.BASE_URL + "/" + dir
+        })),
   });
 });
+
+app.use("/app1", app1)
+app.use("/app2", app2)
 
 app.listen(3000);
